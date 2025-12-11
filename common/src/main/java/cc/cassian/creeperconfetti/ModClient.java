975d859e036d76dc6e3a9c5073dc1abf73d6b341
Cassian Godsted <cassian@godsted.com>
2025-12-10 21:23:46 -0500

    Port to 1.21.11

--- common/src/main/java/cc/cassian/creeperconfetti/ModClient.java
+++ common/src/main/java/cc/cassian/creeperconfetti/ModClient.java
@@ -1,8 +1,8 @@
 package cc.cassian.creeperconfetti;
 
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.sounds.SoundEvent;
 
 public class ModClient {
-  public static SoundEvent confetti = SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath("creeperconfetti", "confetti"));
+  public static SoundEvent confetti = SoundEvent.createVariableRangeEvent(Identifier.fromNamespaceAndPath("creeperconfetti", "confetti"));
 }
